0f8725c64c06e5cd89a8c81e5225415c0c8d890f
raviranjan-ds <raviranjan.career@gmail.com>
2025-12-26 18:55:18 +0530

    Create emoji_converter.py
    This is a beginner-friendly Python project that converts certain words into emojis.
    It demonstrates the use of string replacement, basic Python syntax, and logic building.
    Perfect for learning Python basics and practicing coding.

---
+++ emoji_converter.py
@@ -0,0 +1,11 @@
+text = input("Enter your text:").lower()
+
+text = text.replace("happy", "😊")
+text = text.replace("sad","😒")
+text = text.replace("angry","😡")
+text = text.replace("love","❤️")
+text = text.replace("smile","😇")
+text = text.replace("hiii","Radheradhe😊")
+text = text.replace("hello","jai shree krishna ji🙏")
+
+print(text)
